0a75a5cf577738c28df459eb3381bb62b7d0574d
TomyVillalba <36230596+TomyVillalba@users.noreply.github.com>
2018-02-15 12:18:49 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -5,8 +5,18 @@ var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
 	
-	
 
+    switch (mesDelAño)
+   {
+        case "Enero": alert ("Este mes tiene 31 dias");
 
+        case "Febrero": alert("Este mes tiene 28 dias");
+                        break;
+       default: alert ("Este mes tiene 30 o mas dias");
+                break;  
+   case "Enero": alert ("Este mes tiene 31 dias");
+        default            
+	   
+   }
 
 }//FIN DE LA FUNCIÓN

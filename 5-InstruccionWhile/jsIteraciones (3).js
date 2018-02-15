48491eb2d5368b3ad7de49d358a4ca688ef60fce
TomyVillalba <36230596+TomyVillalba@users.noreply.github.com>
2018-02-15 12:17:51 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -1,7 +1,12 @@
 function Mostrar()
 {
 
-var clave = prompt("ingrese el número clave.");
+var clave = prompt("ingrese el número clave");
 
+        while (clave != "utn750")
+        {
+            clave = prompt("Esta contraseña no es valida, por favor ingresela nuevamente");
+        }
+         alert("Clave correcta, bienvenido");
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -3,5 +3,10 @@ function Mostrar()
 
 	var numero = prompt("ingrese un número entre 0 y 10.");
 
+     while(numero >= 9 || numero <= 0)
 
+	 {
+		numero = prompt("Ingrese un numero entre 0 y 10");
+	 }
+	 	document.getElementById("Numero");
 }//FIN DE LA FUNCIÓN

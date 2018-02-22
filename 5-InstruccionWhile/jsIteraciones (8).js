71ed1e61ad9fda8ead6353de2a9e2db485c0249f
TomyVillalba <36230596+TomyVillalba@users.noreply.github.com>
2018-02-21 21:33:18 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -9,15 +9,30 @@ function Mostrar()
     
 	do
 	{
-		numero = parseInt(prompt("¿Desea continuar?"));
+		numero = parseInt(prompt("Ingrese un numero"));
 		if(numero >= 0)
 		{
-			
+			negativo = negativo * numero;
+			flag = 1;
 		}
-	
-    }
+		else
+		{
+			positivo = positivo + numero;
+		}
+	    respuesta = prompt("Desea continuar?: ");
+	}
+	while (respuesta == "s");
 
+
+}
 document.getElementById('suma').value=positivo;
+
+if (flag == 0)
+{
+	negativo = 0;
+
+}
+
 document.getElementById('producto').value=negativo;
 
-}//FIN DE LA FUNCIÓN
+//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -2,17 +2,38 @@ function Mostrar()
 {
 
 	var contador=0;
+	var max;
+	var min;
+	var numero;
 	// declarar variables
 	
-	var respuesta='si';
+	var respuesta= "si";
 
-	while(respuesta!='no')
+	do
 	{
-		
-	
+
+		numero = parseInt(prompt("Ingrese un numero: "));
+		contador ++;
+
+		if (contador == 1)
+		{
+			max = numero;
+			min = numero;
+             
+		}
+		if (numero > max)
+		    max = numero;
+		{
+			if (numero < min)
+			min = numero;
+			
+		}
+	     respuesta = prompt("Desea continuar?: ");
 	}
+        while (respuesta == "si");
 
-
+		document.getElementById("maximo").value = max;
+		document.getElementById("minimo").value = min;
 
 
 }//FIN DE LA FUNCIÓN

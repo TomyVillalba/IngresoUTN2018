412261d6587f3e0ee14d32d46dcc410571e95891
TomyVillalba <36230596+TomyVillalba@users.noreply.github.com>
2018-02-22 10:29:16 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,18 +1,65 @@
 function Mostrar()
 {
-
-	var contador=0;
-	//declarar contadores y variables 
-	
+    var numero;
+    var sumaPos=0;
+	var sumaNeg=0;
+	var cantPos=0;
+	var cantNeg=0;
+	var cantCeros=0;
+	var cantPares=0;
+	var promPos;
+	var promNeg;
+	var diferencia; 
 	var respuesta="si";
-
-	while(respuesta!="no")
-	{
-		
+do
+{
+	numero = parseInt(prompt("Ingrese un numero: "));
 	
+	if (numero > 0)
+	{
+		sumaPos = sumaPos + numero;
+		cantPos ++;
+		
 	}
+	else if (numero < 0)
+		{
+			sumaNeg = sumaNeg + numero;
+			cantNeg ++;
+		}
+		else if (numero == 0)
+			{
+                 cantCeros++;
+		
+		    }
+			
+				if(numero % 2 == 0)
+				{
+					cantPares ++;
+				}
+				
+	respuesta = prompt("Desea continuar?: ");
+}while(respuesta!="no");
+	
+		
+	promPos = sumaPos / cantPos;
+	promNeg = sumaNeg / cantNeg;
+	diferencia = sumaPos - sumaNeg;
 
 
 
+
+    
+	document.write("Suma de los positivos" + sumaPos + "<br/>");
+	document.write("Suma de los negativos" + sumaNeg + "<br/>");
+	document.write("Cantidad de positivos" + cantPos + "<br/>");
+	document.write("Cantidad de negativos" + cantNeg + "<br/>");
+	document.write("Cantidad de ceros" + cantCeros + "<br/>");
+	document.write("Cantidad de pares" + cantPares + "<br/>");
+	document.write("Promedio de positivos" + promPos + "<br/>");
+	document.write("Promedio de negativos" + promNeg + "<br/>");
+	document.write("Diferencia de positivos" + diferencia + "<br/>");
+	
+
+		
 
 }//FIN DE LA FUNCIÓN

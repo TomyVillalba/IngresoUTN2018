ff1aa4d3093f193586f21fe7bccbd5967851be90
TomyVillalba <36230596+TomyVillalba@users.noreply.github.com>
2018-02-28 12:35:19 -0300

    Add files via upload

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -9,9 +9,36 @@ function Sumar ()
 }
 function Promedio () 
 {
-	
+var precio1;
+var precio2;
+var precio3;
+var total;
+var promedio;
+
+precio1 = parseFloat(document.getElementById("PrecioUno").value);
+precio2 = parseFloat(document.getElementById("PecioDos")).value;
+precio3 = parseFloat(document.getElementById("PrecioTres")).value;
+
+total = precio1 + precio2 + precio3;
+
+promedio = total / 3
+
+alert("El promedio de los precios es $ " + promedio);
+
 }
 function PrecioFinal () 
 {
-	
+var precio1;
+var precio2;
+var precio3;
+var total;
+var precioIva;
+
+precio1 = parseFloat(document.getElementById("PrecioUno").value);
+precio2 = parseFloat(document.getElementById("PecioDos")).value;
+precio3 = parseFloat(document.getElementById("PrecioTres")).value;
+
+total = precio1 + precio2 + precio3;
+
+precioMasIva = 
 }

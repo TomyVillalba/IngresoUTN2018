35d2ee6b5905b7b5a9ab5219904a068a92374766
TomyVillalba <36230596+TomyVillalba@users.noreply.github.com>
2018-02-28 09:17:03 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (6).js
+++ 6-InstruccionFor/jsIteracionesFor (6).js
@@ -1,7 +1,20 @@
 function Mostrar()
 {
+   var contPares=0;
+   var i;
+   var num;
+   var esPar;
 
-
+   numero = parseInt(prompt("Ingrese un numero"));
+   for(i = 1; i <= numero; i ++)
+   {
+       if(i % 2 == 0)
+       {
+           contPares ++;
+           console.log(i)
+       }
+   }
+console.log("Se encontraron " + contPares + " numeros pares")
 
 
 }//FIN DE LA FUNCIÓN
--- 6-InstruccionFor/jsIteracionesFor (7).js
+++ 6-InstruccionFor/jsIteracionesFor (7).js
@@ -1,6 +1,20 @@
 function Mostrar()
 {
+var contDivisores=0;
+   var i;
+   var num;
+   var esDivisor;
 
+   numero = parseInt(prompt("Ingrese un numero"));
+   for(i = 2; i <= numero; i ++)
+   {
+       if(i % 2 == 0)
+       {
+           contPares ++;
+           console.log(i)
+       }
+   }
+console.log("Se encontraron " + contDivisores + " divisores")
 
 
 

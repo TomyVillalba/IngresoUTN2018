eb6462e9a78d820fec2ea523821a963bd9ed3081
TomyVillalba <36230596+TomyVillalba@users.noreply.github.com>
2018-03-01 12:21:21 -0300

    Add files via upload

--- ExamenIngresoViejo/codigoFuenteParcial/6-iteraciones.js
+++ ExamenIngresoViejo/codigoFuenteParcial/6-iteraciones.js
@@ -1,6 +1,28 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	
+    var venta;
+    var ventaMayor;
+    var ventaMenor;
+    var flag = 0;
+
+    for(var i = 1; i <= 7; i ++)
+    {
+        venta = parseFloat(prompt("Ingrese importe de venta "));
+    }
+    while(venta <= 0)
+    {
+        venta = parseFloat(prompt("Error. El importe debe ser mayor a 0. Reingrese importe"));
+    }
+    if(venta > ventaMayor || flag == 0)
+    {
+        ventaMayor = venta;
+    }
+    if(venta < ventaMenor || flag == 0)
+    {
+        ventaMenor = venta;
+        flag = 1;
+    }
+    alert("La mayor venta fue $ " + ventaMayor + " y la menor venta $ " + ventaMenor);
 }
 
--- ExamenIngresoViejo/codigoFuenteParcial/7-iteraciones.js
+++ ExamenIngresoViejo/codigoFuenteParcial/7-iteraciones.js
@@ -1,6 +1,15 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	
+    var notasTotales;
+    var notasBaja;
+    var varonesMas6 = 6;
+    var promedio;
+    var contAlumnos = 0;
+    var flag =0;
+
+    for(var i = )
+
+
 }
 

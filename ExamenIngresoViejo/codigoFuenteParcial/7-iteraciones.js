a028ab83d2399f047db75aeb511da91dae34b950
TomyVillalba <36230596+TomyVillalba@users.noreply.github.com>
2018-03-01 23:29:17 -0300

    Add files via upload

--- ExamenIngresoViejo/codigoFuenteParcial/7-iteraciones.js
+++ ExamenIngresoViejo/codigoFuenteParcial/7-iteraciones.js
@@ -7,8 +7,18 @@ function Mostrar()
     var promedio;
     var contAlumnos = 0;
     var flag =0;
+    var nota;
 
-    for(var i = )
+    for(var i = 0; i < 6;i ++)
+        {
+            nota = parseInt(prompt("Indgrese nota "));
+        }
+        while (nota < 0 || nota > 10)
+        {
+            nota = parseInt(prompt("Error. La nota debe entre 0 y 10"));
+        }
+
+        sexo = prompt("Ingrese sexo ");
 
 
 }
